72dd082d8cb8a90f0f8a1a03947d9b446378c47c
turekjesse <turekjesse@gmail.com>
2021-06-09 18:02:56 -0400

    mvp complete, emoji array added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // DOM ELEMENTS
+// const resumeBtn = document.querySelector('.resume-btn')
+// const pauseBtn = document.querySelector('.pause-btn')
 const squares = document.querySelectorAll('.square')  // array
-const moji = document.querySelectorAll('.moji')
 const score = document.querySelector('#score')
 const time = document.querySelector('#time')
 const startBtn = document.querySelector('.start-btn')
-const resumeBtn = document.querySelector('.resume-btn')
-const pauseBtn = document.querySelector('.pause-btn')
 const overlay = document.querySelector('.overlay')
+const gameResults = document.querySelector('.game-results')
 
 // GLOBAL VARS
 let mojiSquare;
@@ -15,20 +15,21 @@ let startTime = 30;
 let timer = null;
 let timeDown = null;
 
+let emojis = ['😀','😁','😂','😃','😄','😅','😆','😇','😈','😉','😊','😋','😌','😍','😎','😏','😐','😑','😒','😓','😔','😕','😖','😗','😘','😙','😚','😛','😜','😝','😞','😟','😠','😡','😢','😣','😤','😥','😦','😧','😨','😩','😪','😫','😬','😭','😮','😯','😰','😱','😲','😳','😴','😵','😶','😷','🙁','🙂','🙃','🙄','🤐','🤑','🤒','🤓','🤔','🤕','🤠','🤡','🤢','🤣','🤤','🤥','🤧','🤨','🤩','🤪','🤫','🤬','🤭','🤮','🤯','🧐',
+]
 // FUNCTIONS
 
-// 
 function randomSquare() {
     squares.forEach(square => {
-        square.classList.remove('moji')
+        square.innerHTML = ''
     })
     let randomSquare = squares[Math.floor(Math.random()*9)]
-    randomSquare.classList.add('moji')
+    randomSquare.innerText = emojis[Math.floor(Math.random()*82)]
     mojiSquare = randomSquare.id
 }
 
 squares.forEach(square => {
-    square.addEventListener('click', () => {
+    square.addEventListener('mousedown', () => {
         if (square.id == mojiSquare){
             total++
             score.innerHTML = `SCORE: 0${total}`
@@ -39,6 +40,12 @@ squares.forEach(square => {
     })
 })
 
+function clearSquares() {
+    squares.forEach(square => {
+        square.innerHTML = ''
+    });
+}            
+
 function updateTime() {
     startTime --
     time.innerHTML = `TIME:  0${startTime}`
@@ -48,6 +55,8 @@ function updateTime() {
     if (startTime === 0) {
         clearInterval(timeDown)
         clearInterval(timer)
+        clearSquares()
+        endGame()
     }
 };
 
@@ -56,7 +65,7 @@ function callUpdateTime() {
 }    
 
 function moveMoji() {
-    timer = setInterval(randomSquare, 800)
+    timer = setInterval(randomSquare,750)
 }
 
 function startGame() {
@@ -65,6 +74,16 @@ function startGame() {
     callUpdateTime()
 }
 
+function endGame() {
+    const restartBtn = document.createElement('button')
+    restartBtn.innerText = 'RESTART'
+    restartBtn.setAttribute('class', 'restart')
+    restartBtn.addEventListener('click', () => document.location.reload())
+    gameResults.innerHTML = `
+        <h2>FINAL SCORE: ${total}</h2>`
+    gameResults.appendChild(restartBtn); 
+    restartBtn.addEventListener('click', () => document.location.reload())
+    }
 
-// EVENT LISTENERS
+    // EVENT LISTENERS
 startBtn.addEventListener('click', startGame)
