88688591dadd3391092e58cafd6a6381c3894b00
turekjesse <turekjesse@gmail.com>
2021-06-09 11:27:22 -0400

    buttons selected from dom, start game function working

--- js/app.js
+++ js/app.js
@@ -3,9 +3,15 @@ const squares = document.querySelectorAll('.square')  // array
 const moji = document.querySelectorAll('.moji')
 const score = document.querySelector('#score')
 const time = document.querySelector('#time')
+const startBtn = document.querySelector('.start-btn')
+const resumeBtn = document.querySelector('.resume-btn')
+const pauseBtn = document.querySelector('.pause-btn')
+const overlay = document.querySelector('.overlay')
 
 // GLOBAL VARS
-
+let mojiPosition;
+let total = 0;
+let startTime = 30;
 
 // FUNCTIONS
 
@@ -17,14 +23,28 @@ const randomSquare = () => {
     let randomSquare = squares[Math.floor(Math.random()*9)]
     // console.log(randomSquare)
     randomSquare.classList.add('moji')
+    mojiPosition = randomSquare.id
 }
+
+squares.forEach(square => {
+    square.addEventListener('click', () => {
+        if (square.id == mojiPosition){
+            total++
+            score.innerHTML = total
+        }
+    })
+})
  
 const moveMoji = () =>{
     let timer = null;
     timer = setInterval(randomSquare, 800)
 }
 
-// moveMoji()
+const startGame = () => {
+    overlay.style.display = 'none';
+    moveMoji()
+}
 
 
 // EVENT LISTENERS
+startBtn.addEventListener('click', startGame)
